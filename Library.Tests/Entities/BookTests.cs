20376566cc3fbee8132776681607ddd2c8a3c8df
BRHBS0593 <BRHBS0593@ambev.com.br>
2020-09-06 22:24:11 -0300

    - Add test;

---
+++ Library.Tests/Entities/BookTests.cs
@@ -0,0 +1,33 @@
+﻿using Library.Domain.Interfaces;
+using Library.Domain.Model;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Library.Tests.Entities
+{
+    public class BookTests
+    {
+        private IServiceBook _mock;
+
+        public BookTests()
+        {
+            _mock = Substitute.For<IServiceBook>();
+
+            var books = new List<BookModel>() { new BookModel(0, "bookName", "description", "author") };
+
+            _mock.RecoverAll().Returns(books);
+        }
+
+        [Fact]
+        public void ShouldReturnErroWhenBookNotExist()
+        {
+            var book = new BookModel(1, "bookName1", "description", "author");
+
+            var allBooks = _mock.RecoverAll();
+
+            Assert.Null(allBooks.Where(x => x.Id == book.Id).FirstOrDefault());
+        }
+    }
+}
---
+++ Library.Tests/Entities/UserTests.cs
@@ -0,0 +1,34 @@
+﻿using Library.Domain.Entities;
+using Library.Domain.Interfaces;
+using Library.Domain.Model;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Library.Tests.Entities
+{
+    public class UserTests
+    {
+        private IServiceUser _mock;
+
+        public UserTests()
+        {
+            _mock = Substitute.For<IServiceUser>();
+
+            var users = new List<UserModel>() { new UserModel(0, "admin", "admin@admin.com.br", "admin", true) };
+
+            _mock.RecoverAll().Returns(users);
+        }
+
+        [Fact]
+        public void ShouldReturnErroWhenBookNotExist()
+        {
+            var user = new UserModel(1, "usernew", "usernew@usernew.com.br", "usernew", true);
+
+            var allUsers = _mock.RecoverAll();
+
+            Assert.Null(allUsers.Where(x => x.Id == user.Id).FirstOrDefault());
+        }
+    }
+}
